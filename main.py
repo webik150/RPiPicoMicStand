05a3ba237b73f3656103ec753027b3818c6e3e59
webik150 <krystof@webik150.com>
2024-09-09 20:15:41 +0200

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿from machine import Pin
+import time
+
+led = Pin("LED", Pin.OUT)
+
+led.value(1)
+
+# flash the led quickly 10 times
+for i in range(10):
+    led.value(not led.value())
+    time.sleep(0.3)
